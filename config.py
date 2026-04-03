bc9f8074c6e7114b6b2e5e2a2e9dc015ebce75ba
hossainm10 <hossain.mahatheer@uconn.edu>
2026-04-02 22:57:56 -0400

    Finished config.py

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
-all_matches_url="https://free-api-live-football-data.p.rapidapi.com/football-get-all-matches-by-league?leagueid=42"
+import os
+from dotenv import load_dotenv
 
+
+
+
+base_path=os.path.dirname(__file__)
+env_path=os.path.join(base_path, ".env")
+
+load_dotenv(env_path)
+
+freeapi_url= "https://free-api-live-football-data.p.rapidapi.com"
+
+
+
+
+free_api=os.getenv("free_api")
 
